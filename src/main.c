43cba426d7d0b8becec656f05f2e30e61fc4b750
calint <calin.tenitchi@gmail.com>
2015-04-05 12:43:39 -0500

    .

--- src/main.c
+++ src/main.c
@@ -16,25 +16,17 @@ static void key_callback(GLFWwindow*window, int key,int scancode,int action,int 
 int main(int argc,char**argv){
 	while(argc--)puts(*argv++);
 	puts(glfwGetVersionString());
-	if(!glfwInit())
-		return 1;
+	if(!glfwInit())return 1;
 //	glfwWindowHint(GLFW_SAMPLES,4);
 //	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
 //	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
 //	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow*window=glfwCreateWindow(WINDOW_WIDTH,WINDOW_HEIGHT,"gleso",NULL,NULL);
-	if(!window){
-		glfwTerminate();
-		return 2;
-	}
+	if(!window){glfwTerminate();return 2;}
 	glfwSetKeyCallback(window,key_callback);
 	glfwMakeContextCurrent(window);
-
 //	glewExperimental=GL_TRUE; // needed for core profile
-	if (glewInit()!=GLEW_OK) {
-		return 3;
-	}
-
+	if (glewInit()!=GLEW_OK)return 3;
 	gleso_init();
 	gleso_on_viewport_change(WINDOW_WIDTH,WINDOW_HEIGHT);
 	while(!glfwWindowShouldClose(window)){
