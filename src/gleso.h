e5ea1072928a44e8104a368c0803cf39033bb33e
calint <calin.tenitchi@gmail.com>
2015-04-09 21:29:20 -0500

    .

--- src/gleso.h
+++ src/gleso.h
@@ -13,9 +13,9 @@ extern "C" {
 	int gleso_init();// called when opengl context needs reload
 	void gleso_viewport(const int width,const int height);// called when screen size changes
 	void gleso_step();// called when opengl ready for render
-	void gleso_key(int key,int scancode,int action,int mods);// called on key event
+	void gleso_key(const int key,const int scancode,const int action,const int mods);// called on key event
 	//action 0:press   2:drag   1:release
-	void gleso_touch(floato x,floato y,int action);// called on touch/pointer event
+	void gleso_touch(const floato x,const floato y,const int action);// called on touch/pointer event
 	void gleso_cleanup();
 #ifdef __cplusplus
 }
