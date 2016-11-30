c81f9988f747f45adcceb7133de86572faf4c909
calint <calin.tenitchi@gmail.com>
2016-11-30 14:32:49 +0700

    .

--- make.sh
+++ make.sh
@@ -8,9 +8,11 @@
 CC='g++ -std=c++17'
 SRC=src/platform-linux/main.cpp
 BIN=gleso
-WARNINGS=-'Wall -Wextra -pedantic -Wfatal-errors -Wno-unused-function -Wno-unused-parameter'
+WARNINGS='-Wall -Wextra -pedantic -Wfatal-errors -Wno-unused-function -Wno-unused-parameter'
 LIBS='-lGL -lglfw -lGLEW -lpthread'
 OPTIMIZATIONS=-O3
+VALGRIND='valgrind --leak-check=full --track-origins=yes'
 
 $CC $SRC -o $BIN $OPTIMIZATIONS $LIBS $WARNINGS &&
-ls -la --color gleso
+ls -la --color $BIN &&
+$VALGRIND ./$BIN
